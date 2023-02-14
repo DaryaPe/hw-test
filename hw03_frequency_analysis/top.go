722802b930023627e827c9f37b848ce46f163074
Darya Pelipenko <darya.pelipenko@south.rt.ru>
2023-02-14 21:16:48 +0300

    HW3 is completed fix

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -8,66 +8,35 @@ import (
 
 var reg = regexp.MustCompile(`([а-я]+[\-а-я]*)+`)
 
-type Word struct {
-	Value     string
-	Frequency int
-}
-
-func NewWord(value string) *Word {
-	return &Word{
-		Value:     value,
-		Frequency: 1,
-	}
-}
-
-type Words []Word
-
-func (ws Words) Sort() {
-	sort.Slice(ws, func(i, j int) bool {
-		if ws[i].Frequency == ws[j].Frequency {
-			return ws[i].Value < ws[j].Value
-		}
-		return ws[i].Frequency > ws[j].Frequency
-	})
-}
-
-func (ws Words) ToStrings(count int) []string {
-	result := make([]string, 0, count)
-	for i := range ws {
-		result = append(result, ws[i].Value)
-		if i == count-1 {
-			break
-		}
-	}
-	return result
-}
-
-type WordsMap map[string]*Word
-
-func (wm WordsMap) ToSlice() Words {
-	result := make(Words, 0, len(wm))
-	for i := range wm {
-		result = append(result, *wm[i])
-	}
-	return result
-}
-
-func calcWordsFrequency(in []string) WordsMap {
-	m := make(WordsMap, len(in))
+func calcWordsFrequency(in []string) map[string]int {
+	m := make(map[string]int, len(in))
 	for _, str := range in {
-		if m[str] == nil {
-			m[str] = NewWord(str)
-		} else {
-			m[str].Frequency++
-		}
+		m[str]++
 	}
 	return m
 }
 
 func Top10(in string) []string {
 	words := reg.FindAllString(strings.ToLower(in), -1)
-	wordsWithRate := calcWordsFrequency(words)
-	wordsSlice := wordsWithRate.ToSlice()
-	wordsSlice.Sort()
-	return wordsSlice.ToStrings(10)
+	wordsMap := calcWordsFrequency(words)
+
+	wordsSlice := make([]string, 0, len(wordsMap))
+	for i := range wordsMap {
+		wordsSlice = append(wordsSlice, i)
+	}
+
+	sort.Slice(wordsSlice, func(i, j int) bool {
+		valueI := wordsSlice[i]
+		valueJ := wordsSlice[j]
+		if wordsMap[valueI] == wordsMap[valueJ] {
+			return wordsSlice[i] < wordsSlice[j]
+		}
+		return wordsMap[valueI] > wordsMap[valueJ]
+	})
+
+	limit := 10
+	if len(wordsSlice) < 10 {
+		limit = len(wordsSlice)
+	}
+	return wordsSlice[:limit]
 }
