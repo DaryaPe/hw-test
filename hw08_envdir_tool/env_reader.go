9e63504257d494eed0348792c85f6f0da1cdff7e
Darya Pelipenko <darya.pelipenko@south.rt.ru>
2023-05-10 19:17:39 +0300

    HW8 is completed

--- hw08_envdir_tool/env_reader.go
+++ hw08_envdir_tool/env_reader.go
@@ -1,5 +1,15 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+)
+
+var ErrNotValidEnvName = fmt.Errorf("not valid environment name")
+
 type Environment map[string]EnvValue
 
 // EnvValue helps to distinguish between empty files and files with the first empty line.
@@ -11,6 +21,59 @@ type EnvValue struct {
 // ReadDir reads a specified directory and returns map of env variables.
 // Variables represented as files where filename is name of variable, file first line is a value.
 func ReadDir(dir string) (Environment, error) {
-	// Place your code here
-	return nil, nil
+	dirContent, err := os.ReadDir(dir)
+	if err != nil {
+		return nil, err
+	}
+
+	env := make(Environment, len(dirContent))
+	for _, file := range dirContent {
+		if file.IsDir() {
+			continue
+		}
+
+		info, err := file.Info()
+		if err != nil {
+			return nil, err
+		}
+		if err = validateEnvName(info.Name()); err != nil {
+			return nil, err
+		}
+		if info.Size() == 0 {
+			env[file.Name()] = EnvValue{NeedRemove: true}
+			continue
+		}
+
+		value, err := os.Open(dir + "/" + file.Name())
+		if err != nil {
+			return nil, err
+		}
+		env[file.Name()] = readEnvValue(value)
+		err = value.Close()
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return env, nil
+}
+
+func validateEnvName(name string) error {
+	if strings.Count(name, "=") != 0 {
+		return ErrNotValidEnvName
+	}
+	return nil
+}
+
+func readEnvValue(file io.Reader) EnvValue {
+	fileScanner := bufio.NewScanner(file)
+
+	if !fileScanner.Scan() {
+		return EnvValue{NeedRemove: true}
+	}
+	str := fileScanner.Text()
+	str = strings.TrimRight(str, "\t")
+	str = strings.TrimRight(str, " ")
+	str = strings.Replace(str, "\x00", "\n", 1)
+	return EnvValue{Value: str, NeedRemove: false}
 }
--- hw08_envdir_tool/env_reader_test.go
+++ hw08_envdir_tool/env_reader_test.go
@@ -1,7 +1,83 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestReadDir(t *testing.T) {
-	// Place your code here
+	defer func() { _ = deleteTestData() }()
+
+	err := generateTestData()
+	if err != nil {
+		fmt.Printf("generateTestData:%v", err)
+	}
+
+	t.Run("dir not exists", func(t *testing.T) {
+		_, err := ReadDir("testdata/env111")
+		require.ErrorIs(t, err, os.ErrNotExist)
+	})
+	t.Run("not valid env name", func(t *testing.T) {
+		_, err := ReadDir("tests")
+		require.ErrorIs(t, err, ErrNotValidEnvName)
+	})
+	t.Run("ok", func(t *testing.T) {
+		env, err := ReadDir("testdata/env")
+		require.ErrorIs(t, err, nil)
+		envExp := Environment{
+			"BAR": EnvValue{
+				Value:      "bar",
+				NeedRemove: false,
+			},
+			"EMPTY": EnvValue{
+				Value:      "",
+				NeedRemove: false,
+			},
+			"FOO": EnvValue{
+				Value:      "   foo\nwith new line",
+				NeedRemove: false,
+			},
+			"HELLO": EnvValue{
+				Value:      "\"hello\"",
+				NeedRemove: false,
+			},
+			"UNSET": EnvValue{
+				Value:      "",
+				NeedRemove: true,
+			},
+		}
+		require.EqualValues(t, envExp, env)
+	})
+}
+
+func generateTestData() error {
+	err := os.MkdirAll("tests", os.ModePerm)
+	if err != nil {
+		return fmt.Errorf("os.MkdirAll:%w", err)
+	}
+
+	file, err := os.Create("tests/test=test")
+	if err != nil {
+		return fmt.Errorf("os.Create:%w", err)
+	}
+	defer func() {
+		_ = file.Close()
+	}()
+
+	_, err = file.WriteString("testtest")
+	if err != nil {
+		return fmt.Errorf("file.WriteString:%w", err)
+	}
+	return nil
+}
+
+func deleteTestData() error {
+	err := os.RemoveAll("tests")
+	if err != nil {
+		return fmt.Errorf("os.RemoveAll:%w", err)
+	}
+	return nil
 }
